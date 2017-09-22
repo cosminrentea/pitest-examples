20310f2c38717d3dab676187d2cacd6de420acf3
Cosmin Rentea <rentea@adobe.com>
2017-09-22 08:43:46 +0300

    step 1: coverage 100%, careless tests (as evidenced by mutation-testing)

--- mymath/src/test/java/ro/rentea/mutationtesting/MyMathTest.java
+++ mymath/src/test/java/ro/rentea/mutationtesting/MyMathTest.java
@@ -3,38 +3,33 @@ package ro.rentea.mutationtesting;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class MyMathTest {
     private MyMath math = new MyMath();
 
     @Test
     public void should_return_sum_of_parameters() {
-        int sum = math.add(2, 1);
+        int sum = math.add(3, 0);
         assertEquals(sum, 3);
     }
 
     @Test
-    public void should_return_value_under_threshold() {
-        int v = math.valueUnderThreshold(1, 2);
-        assertEquals(v, 1);
-    }
-
-    @Test
-    public void should_return_threshold() {
+    public void should_check_threshold() {
         int v = math.valueUnderThreshold(2, 2);
-        assertEquals(v, 2);
+        assertTrue(v < 3);
     }
 
     @Test
-    public void should_return_threshold_for_larger_value() {
-        int v = math.valueUnderThreshold(3, 2);
-        assertEquals(v, 2);
+    public void should_check_threshold_2() {
+        int v = math.valueUnderThreshold(1, 2);
+        assertTrue(v < 3);
     }
 
     @Test
     public void should_return_gcd() {
-        int gcd = math.gcd(20, 8);
-        assertEquals(gcd, 4);
+        int gcd = math.gcd(8, 8);
+        assertEquals(gcd, 8);
     }
 
 }
