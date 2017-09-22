9250c9bcd7b5e608984abcf8ea3390c0d8f52b4b
Cosmin Rentea <rentea@adobe.com>
2017-09-22 08:47:43 +0300

    step 2: coverage 100%, tests without assertions

--- mymath/src/test/java/ro/rentea/mutationtesting/MyMathTest.java
+++ mymath/src/test/java/ro/rentea/mutationtesting/MyMathTest.java
@@ -11,25 +11,21 @@ public class MyMathTest {
     @Test
     public void should_return_sum_of_parameters() {
         int sum = math.add(3, 0);
-        assertEquals(sum, 3);
     }
 
     @Test
     public void should_check_threshold() {
         int v = math.valueUnderThreshold(2, 2);
-        assertTrue(v < 3);
     }
 
     @Test
     public void should_check_threshold_2() {
         int v = math.valueUnderThreshold(1, 2);
-        assertTrue(v < 3);
     }
 
     @Test
     public void should_return_gcd() {
         int gcd = math.gcd(8, 8);
-        assertEquals(gcd, 8);
     }
 
 }
